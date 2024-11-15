fd7a4be1ac97834a0f451a56ff13e13d0c3f6d26
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-15 15:33:24 +0100

    ADD: es-02

---
+++ hobbies.js
@@ -0,0 +1,5 @@
+function createHobbiesObject(hobbyOne, hobbyTwo, hobbyThree) {
+    return { hobbies: [hobbyOne, hobbyTwo, hobbyThree] };
+}
+
+module.exports = createHobbiesObject;
---
+++ index.js
@@ -0,0 +1,15 @@
+// ES 1 
+const createPerson = require('./names'); // Importa la funzione da names.js
+
+const name = createPerson('Lorenzo', 'Scalvini');
+console.log(name);
+
+//ES 2
+const createHobbiesObject = require('./hobbies'); // Importa la funzione da hobbies.js
+
+const myHobbies = createHobbiesObject('Reading', 'Cycling', 'Gaming');
+console.log(myHobbies); 
+
+//ES 3
+
+
